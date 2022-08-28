3094574621d4d37eb352a46917e0bf5f581dba11
Amit Gupta <amit_@outlook.in>
2022-08-27 22:41:07 -0500

    First commit for python port

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext as build_ext_orig
+import pathlib
+import os
+import subprocess
+
+class CMakeExtension(Extension):
+    def __init__(self, name, cmake_lists_dir='.', **kwa):
+        Extension.__init__(self, name, sources=[], **kwa)
+        self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
+
+class cmake_build_ext(build_ext_orig):
+    def build_extensions(self):
+        # Ensure that CMake is present and working
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError('Cannot find CMake executable')
+
+        for ext in self.extensions:
+            extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+            cfg = 'Release'
+
+            cmake_args = [
+                '-DCMAKE_BUILD_TYPE=%s' % cfg,
+                # containing the extension
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                '-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), self.build_temp),
+                 ]
+            if not os.path.exists(self.build_temp):
+                os.makedirs(self.build_temp)
+            subprocess.check_call(['cmake', ext.cmake_lists_dir] + cmake_args, cwd=self.build_temp)
+            subprocess.check_call(['cmake', '--build', '.', '--config', cfg], cwd=self.build_temp)
+
+
+mdstresslab_bindings = CMakeExtension(
+    "pymdstresslab.pymdstresslab",
+    cmake_lists_dir="./")
+
+
+setup(
+    name="pymdstresslab",
+    version="0.0.1",
+    author="Nikhil Admal",
+    ext_modules=[mdstresslab_bindings],
+    cmdclass = {'build_ext': cmake_build_ext}
+)
---
+++ src/python_bindings.cpp
@@ -0,0 +1,91 @@
+#include "pybind11/numpy.h"
+#include "pybind11/pybind11.h"
+#include "pybind11/eigen.h"
+#include "typedef.h"
+
+#include <string>
+
+#include "BoxConfiguration.h"
+#include "kim.h"
+#include "Grid.h"
+#include "Sphere.h"
+#include "Stress.h"
+#include "calculateStress.h"
+#include "SubConfiguration.h"
+#include "Stencil.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(pymdstresslab, m){
+    py::class_<Configuration>(m,"Configuration")
+            .def(py::init<int,int>());
+
+    py::class_<BoxConfiguration, Configuration>(m, "BoxConfiguration")
+            .def(py::init<int,int>())
+            //  .def(py::init<std::string, int>())
+            .def("read",&BoxConfiguration::read)
+            .def("getConfiguration",&BoxConfiguration::getConfiguration);
+
+    py::class_<Kim>(m, "Kim")
+            .def(py::init<>())
+            .def(py::init<std::string>());
+
+    py::class_<GridBase>(m, "GridBase")
+            .def(py::init<>());
+
+    py::class_<Grid<Current>,GridBase>(m, "GridCurrent")
+            .def(py::init<int>())
+            .def(py::init<Vector3d, Vector3d, int>())
+            .def("read",&Grid<Current>::read)
+            .def("read",&Grid<Current>::write);
+
+    py::class_<Grid<Reference>,GridBase>(m, "GridReference")
+            .def(py::init<int>())
+            .def(py::init<Vector3d, Vector3d, int>())
+            .def("read",&Grid<Reference>::read)
+            .def("read",&Grid<Reference>::write);
+
+//    py::class_<Sphere>(m, "Sphere")
+//            .def(py::init<double>())
+//            .def(py::init<Sphere&>());
+
+//    py::class_<Stress<Sphere,Cauchy> >(m, "StressCauchy")
+//            .def(py::init<std::string,Sphere &, Grid<Current>* >());
+//    py::class_<Stress<Sphere,Piola> >(m, "StressPiola")
+//            .def(py::init<std::string,Sphere &, Grid<Reference>* >());
+
+//    py::class_<Stencil>(m, "Stencil")
+//            .def(py::init<Configuration&>());
+//    py::class_<SubConfiguration,Configuration>(m, "SubConfiguration")
+//            .def(py::init<Stencil &>());
+//    py::class_<InteratomicForces>(m, "InteratomicForces")
+//            .def(py::init<NeighList*>());
+
+    // calculates one Cauchy stress
+//    m.def("calculateStress",[](BoxConfiguration& body, Kim& kim, std::tuple<Stress<Sphere,Cauchy>&> cauchyStress){
+//        calculateStress(body, kim, std::tie(std::get<0>(cauchyStress)));
+//    });
+//
+//    // calculates one Piola stress
+//    m.def("calculateStress",[](BoxConfiguration& body, Kim& kim, std::tuple<Stress<Sphere,Piola>&> piolaStress){
+//        calculateStress(body, kim, std::tie(std::get<0>(piolaStress)));
+//    });
+//
+//    // calculates a couple of Cauchy stresses
+//    m.def("calculateStress",[](BoxConfiguration& body, Kim& kim, std::tuple<Stress<Sphere,Cauchy>&, Stress<Sphere,Cauchy>&> stress){
+//        calculateStress(body, kim, std::tie(std::get<0>(stress),std::get<1>(stress)));
+//    });
+//
+//    // calculates a couple of Piola stresses
+//    m.def("calculateStress",[](BoxConfiguration& body, Kim& kim, std::tuple<Stress<Sphere,Piola>&, Stress<Sphere,Piola>&> stress){
+//        calculateStress(body, kim, std::tie(std::get<0>(stress),std::get<1>(stress)));
+//    });
+//
+//    // calculates a couple of Piola stresses, and a couple of Cauchy stresses
+//    m.def("calculateStress",[](BoxConfiguration& body, Kim& kim,
+//                               std::tuple<Stress<Sphere,Piola>&,  Stress<Sphere,Piola>&>  piolaStress,
+//                               std::tuple<Stress<Sphere,Cauchy>&, Stress<Sphere,Cauchy>&> cauchyStress){
+//        calculateStress(body, kim, std::tie(std::get<0>(piolaStress),std::get<1>(piolaStress)),
+//                                   std::tie(std::get<0>(cauchyStress),std::get<1>(cauchyStress)));
+//    });
+}
